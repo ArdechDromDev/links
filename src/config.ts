9f0ad188a62c1bf77cad883ca4985284e74ac898
Dominique DUPLAN <dominique.duplan@free.fr>
2023-03-03 13:32:26 +0100

    Version initiale

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const SUBTITLE = "Association tech des professionnels du numérique en Drôme Ardèche"
+export const TITLE = "Ardèch Drôm Dev"
+export const TAGLINE = "Rejoins-nous !"
+
+// Contacts
+export const contact = "Restons en contact"
+export const phone = ""
+export const email = "contact@ardechdromedev.org"
---
+++ src/pages/socials/slack.md
@@ -0,0 +1,4 @@
+---
+icon: slack
+url: https://join.slack.com/t/ardech-drom-dev/shared_invite/enQtNDE5MzQ1NzEwODA3LTY1MDNkN2Y3MzA0ODAwMTg1MzhkOTcwOTY4YTRkMWUxMzI0NWMyOTU0ZmYwNjhiOTMwOGE0ZDYyNmVkNTJkZWE
+---
